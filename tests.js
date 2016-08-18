db2c01a51667b566aeef588b01b5b7f25c2b3c98
Ridwan A. Otun <readonegoke@gmail.com>
2016-08-18 12:54:07 +0100

    wrote tests

--- tests.js
+++ tests.js
@@ -4,21 +4,28 @@ var chai = require('chai');
 var assert = chai.assert;
 
 var NotesApplication = require('./lib/notesapplication.js');
-var Note = require('./lib/note.js');
+var Note = require('./lib/notes.js');
+
 
 describe("Note creation works properly", function() {
-    it("assigns author based on the parameter supplied in the constructor", function() {
-        note = new Note("Hello world", "Chidiebere")
-        assert(note.author == "Chidiebere")
-    })
+
+   it("assigns author based on the parameter supplied in the constructor", function() {
+       note = new Note("Hello world", "Ridwan")
+       assert(note.author == "Ridwan")
+   })
+   it("assigns text to note based on the parameter supplied in the constructor", function() {
+       note = new Note("Hello world", "Ridwan")
+       assert(note.content == "Hello world")
+   })
+   it("asserts that empty parameters are not passed in", function () {
+       note = new Note("Hello World", "Ridwan")
+       assert( (note.content != "") || (note.author != ""))
+   })
+   it("asserts that an empty parameter isn't passed into the setNote method", function() {
+       note = new Note("Hello World", "Ridwan")
+       note.setNote("new changes")
+       assert( note.content == "new changes")
+       
+   })
 })
 
-describe("Notes application increments number of notes as notes are added", function() {
-    it("increments the note list as notes are added", function() {
-        note = new Note("Hello world", "Chidiebere");
-        noteapp = new NotesApplication("Chidiebere");
-        assert(noteapp.notelist.length == 0)
-        noteapp.addNote(note)
-        assert(noteapp.notelist.length == 1)
-    })
-})
