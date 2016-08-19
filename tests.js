44f886426f155ca43ffd0370237c7104f8acd35b
Ridwan A. Otun <readonegoke@gmail.com>
2016-08-19 03:35:41 +0100

    completed tests

--- tests.js
+++ tests.js
@@ -9,32 +9,59 @@ var Note = require('./lib/notes.js');
 
 describe("Note creation works properly", function() {
 
-   it("assigns author based on the parameter supplied in the constructor", function() {
-       note = new Note("Hello world", "Ridwan")
-       assert(note.author == "Ridwan")
-   })
-   
-   it("assigns text to note based on the parameter supplied in the constructor", function() {
-       note = new Note("Hello world", "Ridwan")
-       assert(note.content == "Hello world")
-   })
-   
-   it("checks that empty parameters are not passed in", function () {
-       note = new Note("Hello World", "Ridwan")
-       assert( (note.content != "") || (note.author != ""))
-   })
-   
-   it("checks that an empty parameter isn't passed into the setNote method", function() {
-       note = new Note("Hello World", "Ridwan")
-       note.setNote("new changes")
-       assert( note.content == "new changes")
-       
-   })
+  it("assigns author based on the parameter supplied in the constructor", function() {
+    var firstnote = new Note(1,"Ridwan", "this is first content"), noteapp = new NotesApplication();
+    assert(firstnote.author == "Ridwan")
+  });
+});
 
-   it(""
-        note = new NotesApplication()
-        note.create(new Note("A brand new note", "Ridwan"))
-    )
+describe("NotesApplication manipulating notes", function() {
 
-})
+  it("returns empty for a new note application", function () {
+    var noteapp = new NotesApplication();
+    assert(noteapp.list_notes() === "empty")
+  });
+
+  it("returns number of notes created", function() {
+    var firstnote = new Note(1, "Ridwan", "this is first content"), noteapp = new NotesApplication();
+    noteapp.create(firstnote);
+    assert(noteapp.listNotes() === 1);
+  })
+
+  it("is able to get note by id", function() {
+    var firstnote = new Note(1, "Ridwan", "this is first content"), noteapp = new NotesApplication(), secondnote = new Note(2, "Ridwan", "this is second note"), thirdnote = new Note(3, "Adeola", "this is third note");
+    noteapp.create(firstnote);
+    noteapp.create(secondnote);
+    assert(noteapp.get(2) === true);
+  })
+
+  it("returns index of note with specific text", function(){
+    var firstnote = new Note(1, "Ridwan", "this is first content"), noteapp = new NotesApplication();
+    noteapp.create(firstnote)
+    assert(noteapp.search("is") === 1);
+  })
+
+  it("checks that parameter/arguments are not empty", function() {
+    var firstnote = new Note(1, "Ridwan", "this is first content");
+    assert((firstnote.content !== "") && (firstnote.author !== "" ));
+  })
+
+  it("returns no match found if note does not exist", function() {
+    var firstnote = new Note(1, "Ridwan", "this is first content"), noteapp = new NotesApplication();
+    noteapp.create(firstnote);
+    assert(noteapp.delete(7) == "no match found");
+  })
+
+  it("checks if a note was actually edited", function() {
+    var firstnote = new Note(1, "Ridwan", "this is first content"), noteapp = new NotesApplication();
+    noteapp.create(firstnote);
+    assert(noteapp.edit(1, "this is the first changed content") === "this is the first changed content");
+  })
+
+  it("assigns text to note based on the parameter supplied in the constructor", function() {
+    firstnote = new Note(1, "Ridwan", "this is first content");
+    assert(firstnote.content == "this is first content");
+  })
+
+});
 
